303d98d4d24c16696b610d908a9233933242832d
lopertut <artur.matvejev12345@gmail.com>
2024-05-22 20:01:32 +0300

    added some new imports from telegram.ext

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 
 
 logging.basicConfig(
