f54687029857d8755f45c2015dbfccdedf042191
lopertut <artur.matvejev12345@gmail.com>
2024-05-22 20:38:59 +0300

    created help command function

--- main.py
+++ main.py
@@ -10,13 +10,20 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Im a bot, please talk to me!')
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='what you need to know')
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('7017559815:AAG7EZ86xMhVufKTpj16FFgQt7vxhCkUJSY').build()
 
     start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help_command)
+
     application.add_handler(start_handler)
+    application.add_handler(help_handler)
 
     application.run_polling()
